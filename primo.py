c23d6ecf576d3346c0c66a1de0c8c4d7e806da1e
catania <catania@olomedia.it>
2022-09-14 14:25:55 +0200

    Fix push

--- primo.py
+++
@@ -1,9 +0,0 @@
-#!/bin/python3
-
-#Dichiaro le variabili e creo gli input.
-name = str(input("Nome: "))
-eta = str(input("eta: "))
-
-
-#Stampo a video.
-print("Ciao il mio nome è " + name + " ed ho " + eta + " anni. Mi piacissi!")
